a44b35183753ca045ebc51295721a1d63b168a4b
pam- <pam.assogba@gmail.com>
2014-08-29 14:06:38 -0400

    models done

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20140829160407) do
   end
 
   create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
